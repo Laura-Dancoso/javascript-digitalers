5b17199eafcebecf0df5594e0bd9a7f4619bd058
Laura-Dancoso <lauradancoso@gmail.com>
2020-09-29 20:48:34 -0300

    lab 1 done

--- Web/js/lesson-3.js
+++ Web/js/lesson-3.js
@@ -1,1 +1,20 @@
-//*arrays
+//*arrays 
+//formas de inicializar un array 
+//array statico
+//var array1 = [1,2];
+//array dinamico
+//var array2=[];
+//array2[0]=1;
+//array2[1]=2;
+//console.log(array1[1])
+//*Laboratorio 1
+var datos = ["Pepe","Argento",new Date(1965,11,17).toLocaleDateString(),2020-new Date(1965,11,17).getFullYear()];
+//console.log(datos.length);
+//console.log(datos[1]);
+
+//*for
+//si no conozco el nro de iteraciones que tengo que hacer, me conviene el while o do while
+//cuando sé cuantas veces tengo que iterar sí me sirve el for
+for(var i = 0; i<datos.length; i++){
+console.log(datos[i]);
+}
