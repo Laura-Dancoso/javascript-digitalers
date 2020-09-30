be64c4950c7bb7a28c2bed6c5e410ff6a2197755
Laura-Dancoso <lauradancoso@gmail.com>
2020-09-29 22:16:39 -0300

    dividí los labs de las anotaciones de la clase en 2 .js distintos

---
+++ Web/js/labs.js
@@ -0,0 +1,24 @@
+//*Laboratorio 1
+var datos = ["Pepe", "Argento", new Date(1965, 11, 17).toLocaleDateString(), 2020 - new Date(1965, 11, 17).getFullYear()];
+console.log("----------------------Laboratorio 1----------------------");
+console.log(datos.length);
+console.log(datos[1]);
+
+
+//*Laboratorio 2
+console.log("----------------------Laboratorio 2----------------------");
+console.log("-------------------------Parte 1-------------------------");
+for (let i=1; i<101;i++){
+    console.log(i);
+}
+console.log("-------------------------Parte 2-------------------------");
+for (let i = 0; i < datos.length; i++) {
+    console.log(datos[i]);
+}
+
+//*Laboratorio 3
+console.log("----------------------Laboratorio 2----------------------");
+var personas = [{nombre:"Pepe", apellido:"Gonzalez"},{nombre:"Maria", apellido:"Ginobili"},{nombre:"Pablo", apellido:"Perez"},{nombre:"Gimena", apellido:"Baron"},{nombre:"Monica", apellido:"Argento"}];
+
+//*Laboratorio 4
+//*Proyecto integrador
