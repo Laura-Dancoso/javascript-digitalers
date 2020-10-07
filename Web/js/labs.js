b72fa81ef9da46a98c9494a4646605bbe8681104
Laura-Dancoso <lauradancoso@gmail.com>
2020-10-06 21:56:41 -0300

    clase-5 lab 2 finalizado

--- Web/js/labs.js
+++ Web/js/labs.js
@@ -2,3 +2,12 @@
 $('h1').text('Título');
 $('.miParrafo').text('Párrafo.');
 $('#idParrafo').text('Otro párrafo.')
+//*Lab2
+$('#alert').click(function (e) { 
+  e.preventDefault();
+  alert('Haz apretado el botón')
+});
+$('#titulo').click(function (e) { 
+  e.preventDefault();
+  $('h1').text('Haz cambiado el título')
+});
--- Web/js/lesson-5.js
+++ Web/js/lesson-5.js
@@ -1,6 +1,10 @@
 //*jQuery
 //*selectores
 //utilizo text para modificar y acceder al texto de un elemento
-$('#enc').text('con selector de Id con jquery');
-$('p').text('con selector de Etiqueta con jquery');
+/*$('#enc').text('con selector de Id con jquery');
 $('.caja').text('con selector Class con jquery');
+$('p').text('con selector de Etiqueta con jquery');*/
+//* funciones
+$('.miParrafo').hide();
+$('.miParrafo').show();
+//lightbox: plugin
